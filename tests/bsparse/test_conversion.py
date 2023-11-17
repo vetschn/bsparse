581cb76cf578b80e6298fc4775f88a245878f1aa
Nicolas Vetsch <vetschn@iis.ee.ethz.ch>
2023-11-17 16:41:29 +0000

    Unit tests

--- tests/bsparse/test_conversion.py
+++ tests/bsparse/test_conversion.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pytest
+import scipy.sparse as sp
 
 from bsparse import BCOO, BCSR, BDIA
 from bsparse.bsparse import BSparse
@@ -87,3 +88,61 @@ def test_tocoo(sizes: tuple[np.ndarray, np.ndarray], bsparse_type: BSparse):
 
     assert isinstance(coo, BCOO)
     assert np.allclose(coo.toarray(), arr)
+
+
+@pytest.mark.parametrize(
+    "sizes",
+    [
+        pytest.param((np.arange(1, 6), np.arange(1, 6)), id="5x5"),
+        pytest.param((np.arange(1, 11), np.arange(1, 6)), id="10x5"),
+        pytest.param((np.arange(1, 6), np.arange(1, 11)), id="5x10"),
+    ],
+)
+@pytest.mark.parametrize(
+    "bsparse_type",
+    [
+        pytest.param(BCOO, id="BCOO"),
+        pytest.param(BCSR, id="BCSR"),
+        pytest.param(BDIA, id="BDIA"),
+    ],
+)
+def test_asbformat(sizes: tuple[np.ndarray, np.ndarray], bsparse_type: BSparse):
+    """Tests the `asbformat` method."""
+    shape = (sizes[0].sum(), sizes[1].sum())
+    spmat = (1 + 1j) * sp.random(*shape, 1.0)
+
+    bsparse = bsparse_type.from_sparray(spmat, sizes)
+
+    for format in ["coo", "csc", "csr", "dia", "dok", "lil"]:
+        new_bsparse = bsparse.asbformat(format)
+        assert new_bsparse[0, 0].format == format
+
+    new_bsparse = bsparse.asbformat("array")
+    assert isinstance(new_bsparse[0, 0], np.ndarray)
+
+
+@pytest.mark.parametrize(
+    "sizes",
+    [
+        pytest.param((np.arange(1, 6), np.arange(1, 6)), id="5x5"),
+        pytest.param((np.arange(1, 11), np.arange(1, 6)), id="10x5"),
+        pytest.param((np.arange(1, 6), np.arange(1, 11)), id="5x10"),
+    ],
+)
+@pytest.mark.parametrize(
+    "bsparse_type",
+    [
+        pytest.param(BCOO, id="BCOO"),
+        pytest.param(BCSR, id="BCSR"),
+        pytest.param(BDIA, id="BDIA"),
+    ],
+)
+def test_bapply(sizes: tuple[np.ndarray, np.ndarray], bsparse_type: BSparse):
+    """Tests the `tocoo` method."""
+    shape = (sizes[0].sum(), sizes[1].sum())
+    arr = np.random.random(shape)
+    mat = bsparse_type.from_array(arr, sizes)
+
+    for func in (np.sin, np.exp, lambda x: x * 2):
+        new_mat = mat.bapply(func, copy=True)
+        assert np.allclose(new_mat.toarray(), func(arr))
